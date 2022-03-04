62bfd9b967009b015042525ce86330c9ae52bd97
Sachinnegi9936167436 <91362318+Sachinnegi9936167436@users.noreply.github.com>
2022-03-04 09:45:35 +0530

    Add files via upload

---
+++ Pow.java
@@ -0,0 +1,33 @@
+/*
+Q1. write a java program to compute power of two using for loop
+Q2. Given a list of marks ranging from 0-100, write a programm to compute and print the numbers of student who have obtain marks
+    1. In tha range of 81-100
+    2. In the range of 61-80
+    3. In the range of 41-60
+    the program should use the min. numbers of if statement
+4
+Q3. wap to make a calculator using switch case u can have 5 operation +,-,*,/,% 
+Q4. Shown below Floyd's Triangle
+1
+2  3
+4  5  6
+7  8  9  10
+11 12--------15
+79----------------91*/
+
+
+public class Pow
+{
+public static void main(String arg[])
+{
+int a=2;
+for(int i=0;i<10;i++)
+{
+a=a*2;
+System.out.println(a);
+}
+}
+}
+
+
+
